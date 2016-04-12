89dbb8d56ac2ed702dc9c79e7a7633c29e3e5c37
svetbg <svetbg@users.noreply.github.com>
2016-04-13 00:35:13 +0300

    Update battle-field-staff.user.js
    Added visualization of the top 5 div dmg

--- battle-field-staff.user.js
+++ battle-field-staff.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Battle Field Staff
 // @include      *www.erepublik.com*
-// @version      0.1.7
+// @version      0.1.8
 // @author       SvetBG
 // @grant        none
 // ==/UserScript==
@@ -249,30 +249,42 @@ $( document ).ready(function() {
     //battleId = 75945
     //pomelo.disconnect()
     var battles = {};battles[bId]=battles[bId]||{};battles[bId][currentZoneId]=battles[bId][currentZoneId]||{}
-    setTimeout(function() {
-        //connectBattleSocket()
-        if("undefined"!=typeof pomelo) {
-            pomelo.on('onMessage', function(data) {
-                var pDiv = parseInt(data.division)
-                var pSide = parseInt(data.side)
-                var pDmg = parseInt(data.msg.damage)
-                battles[bId][currentZoneId][pSide]=battles[bId][currentZoneId][pSide]||{},battles[bId][currentZoneId][pSide][pDiv]=battles[bId][currentZoneId][pSide][pDiv]||0;
-                battles[bId][currentZoneId][pSide][pDiv]+=pDmg
-                
-                if (pSide==leftBattleId&&pDiv==fighterDivision){console.log(data.name+': '+pDmg)}
-            })
+    
+    //connectBattleSocket()
+    if("undefined"!=typeof pomelo) {
+        pomelo.on('onMessage', function(data) {
+            var pDiv = parseInt(data.division)
+            var pSide = parseInt(data.side)
+            var pDmg = parseInt(data.msg.damage)
+            battles[bId][currentZoneId][pSide]=battles[bId][currentZoneId][pSide]||{},battles[bId][currentZoneId][pSide][pDiv]=battles[bId][currentZoneId][pSide][pDiv]||0;
+            battles[bId][currentZoneId][pSide][pDiv]+=pDmg
 
-            pomelo.on('onError', function(data) {
-                console.log('Error')
-                console.log(data)
-            })
+            if (pSide==leftBattleId&&pDiv==fighterDivision){console.log(data.name+': '+pDmg)}
+        })
 
-            pomelo.on('onAdd', function(data) {
-                console.log('onAdd')
-                console.log(data)
-            })
+        pomelo.on('onError', function(data) {
+            console.log('Error')
+            console.log(data)
+        })
+
+        pomelo.on('onAdd', function(data) {
+            console.log('onAdd')
+            console.log(data)
+        })
+    }
+    
+    var divDmgInfoCont=$('div#pvp')
+    divDmgInfoCont.after('<div class="div_dmg_left" style="width:100px;position:absolute;top:0px;z-index:350;left:-120px;color:black;">123</div>')
+    divDmgInfoCont.after('<div class="div_dmg_right" style="width:100px;position:absolute;top:0px;right:-120px;z-index:350;color:black;">321</div>')
+    
+    setInterval(function(){
+        var leftI='',rightI='';
+        for (var i=1;i<5;i++) {
+            leftI+='Div'+i+': '+((battles[bId][currentZoneId][leftBattleId]&&battles[bId][currentZoneId][leftBattleId][i])||0).toLocaleString()+'<br />'
+            rightI+='Div'+i+': '+((battles[bId][currentZoneId][rightBattleId]&&battles[bId][currentZoneId][rightBattleId][i])||0).toLocaleString()+'<br />'
         }
-    }, 1)
+        $('.div_dmg_left').html(leftI);$('.div_dmg_right').html(rightI)
+    }, 1e3)
 });
 
 /*
