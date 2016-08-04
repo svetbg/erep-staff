b2167f73bd0af3aae6d1be770db9767939919e8f
svetbg <svetbg@users.noreply.github.com>
2016-08-04 14:36:19 +0300

    Update profile-improvements-dev.user.js
    Cosmetic changes

--- profile-improvements-dev.user.js
+++ profile-improvements-dev.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Profile Improvements
 // @include      *www.erevollution.com/*/profile/*
-// @version      0.0.1
+// @version      0.0.2
 // @description  Profile Improvements
-// @author       Svetlin Tsvetanov <svetbg@gmail.com>
+// @author       SvetBG
 // @grant        none
 // ==/UserScript==
 
@@ -14,6 +14,7 @@ function style(t) {
 (function() {
     'use strict';
     style("select {display:table-cell;}");
+    style(".hits {color: #595959;}");
     var userLang = navigator.language||navigator.userLanguage||'en-US'
     var ranks = ["Recruit","Junior Cadet","Cadet","Cadet Senior","Cadet 1st Class","Soldier","Private Basic","Private 2nd Class","Private","Private 1st Class","Specialist","Gunnery Specialist","Technical Specialist","Specialist 1st Class","Ranger","Lance Corporal","Corporal","Fireteam Leader","Sergeant 3rd Class","Sergeant 2nd Class","Sergeant","Sergeant 1st Class","Staff Sergeant","Gunnery Sergeant","Master Sergeant","First Sergeant","Command Sergeant","Master Gunnery Sergeant","Sergeant Major","Company Sergeant","Warrant Officer Candidate","Warrant Officer","Chief Warrant Officer","Chief Warrant Officer 1st Class","Master Warrant Officer","Quartermaster","Officer Cadet Junior","Officer Cadet Senior","Ensign","Second Lieutenant","First Lieutenant","Lieutenant Captain","Lieutenant","Lieutenant Colonel","Captain Lieutenant","Captain","Captain*","Captain**","Captain***","Colonel","Brigadier","Field Marshal","Commander","High Commander","Supreme Commander","Major General","Lieutenant General","General-Field Marshal","General","SF First Lieutenant","SF Sublieutenant","SF Lieutenant","SF Lieutenant-Colonel","SF Lieutenant-Captain","SF Captain","TSF Captain*","SF Captain**","SF Captain***","SF Colonel","Warface"]
     $( document ).ready(function() {
@@ -35,8 +36,8 @@ function style(t) {
         var tpRemainingHits = 0
         var allyRemainingHits = 0
         
-        tpContainer.after('<strong id="tp_in"></strong>')
-        allyContainer.after('<strong id="ally_in"></strong>')
+        tpContainer.after('<strong id="tp_in" class="hits"></strong>')
+        allyContainer.after('<strong id="ally_in" class="hits"></strong>')
         
         var tpInfo = getDmg(tpContainer)
         var allyInfo = getDmg(allyContainer)
@@ -47,7 +48,7 @@ function style(t) {
         
         calculate()
         
-        $('div.vs164-11').after('<h3 style="margin-top: 15px;clear: both;"><span>Calculator</span></h3><div class="vs165"><strong class="vs165-1">NE: <input type="checkbox" id="ne" value="1.1"/></strong><strong class="vs165-1"><select required="required" class="form-control" id="weapon"><option value="1.0">No weapon</option><option value="1.2">Q1 Weapon</option><option value="1.4">Q2 Weapon</option><option value="1.6">Q3 Weapon</option><option value="1.8">Q4 Weapon</option><option value="2.0">Q5 Weapon</option><option value="1.6">Q1 Tank</option><option value="1.8">Q2 Tank</option><option value="2.2">Q3 Tank</option><option value="2.6">Q4 Tank</option><option value="3.0">Q5 Tank</option><option value="2.4">Q1 Helicopter</option><option value="2.6">Q2 Helicopter</option><option value="3.0">Q3 Helicopter</option><option value="3.5">Q4 Helicopter</option><option value="4.0">Q5 Helicopter</option><option value="5.0">RPG</option></select></strong><strong class="vs165-1"><select required="required" class="form-control" id="dsystem"><option value="0.0">No Defense</option><option value="0.05">Q1 Defense</option><option value="0.10">Q2 Defense</option><option value="0.15">Q3 Defense</option><option value="0.20">Q4 Defense</option><option value="0.25">Q5 Defense</option></select></strong><strong class="vs165-1"><select required="required" class="form-control" id="booster"><option value="1.0">No booster</option><option value="1.1">Q1 Booster</option><option value="1.3">Q3 Booster</option><option value="1.5">Q5 Booster</option></select></strong><h4 id="oneHit" class="vs165-2">'+(oneHit).toLocaleString(userLang, {minimumFractionDigits: 2})+' / hit</h4></div>')
+        $('div.vs164-11').after('<h3 style="margin-top: 15px;clear: both;"><span>Calculator</span></h3><br /><div class="vs165"><strong class="vs165-1">NE: <input type="checkbox" id="ne" value="1.1"/></strong><strong class="vs165-1"><select required="required" class="form-control" id="weapon"><option value="1.0">No weapon</option><option value="1.2">Q1 Weapon</option><option value="1.4">Q2 Weapon</option><option value="1.6">Q3 Weapon</option><option value="1.8">Q4 Weapon</option><option value="2.0">Q5 Weapon</option><option value="1.6">Q1 Tank</option><option value="1.8">Q2 Tank</option><option value="2.2">Q3 Tank</option><option value="2.6">Q4 Tank</option><option value="3.0">Q5 Tank</option><option value="2.4">Q1 Helicopter</option><option value="2.6">Q2 Helicopter</option><option value="3.0">Q3 Helicopter</option><option value="3.5">Q4 Helicopter</option><option value="4.0">Q5 Helicopter</option><option value="5.0">RPG</option></select></strong><strong class="vs165-1"><select required="required" class="form-control" id="dsystem"><option value="0.0">No Defense</option><option value="0.05">Q1 Defense</option><option value="0.10">Q2 Defense</option><option value="0.15">Q3 Defense</option><option value="0.20">Q4 Defense</option><option value="0.25">Q5 Defense</option></select></strong><strong class="vs165-1"><select required="required" class="form-control" id="booster"><option value="1.0">No booster</option><option value="1.1">Q1 Booster</option><option value="1.3">Q3 Booster</option><option value="1.5">Q5 Booster</option></select></strong><h4 id="oneHit" class="vs165-2 hits">'+(oneHit).toLocaleString(userLang, {minimumFractionDigits: 2})+' / hit</h4></div>')
         
         
         function calculate()
