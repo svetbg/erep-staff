69d10d59bec1fdaf71a1bdb0f3e7b7020779e815
svetbg <svetbg@users.noreply.github.com>
2016-08-05 08:32:25 +0300

    Update homepage-improvements-dev.user.js

--- homepage-improvements-dev.user.js
+++ homepage-improvements-dev.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Erev HomePage Improvements
 // @include      *www.erevollution.com/*/index
-// @version      0.0.3
+// @version      0.0.4
 // @description  Erev HomePage Improvements
 // @author       Anonymous
 // @grant        none
@@ -19,22 +19,26 @@ function style(t) {
     $( document ).ready(function() {
         var recoverableEnergy = 0
         
-        var checkEnergyInterval = setInterval(checkEnergy, 6e4)
+        var checkEnergyInterval = setInterval(checkEnergy, 6*6e4)
         setTimeout(checkEnergy, 10e2)
         
         function checkEnergy() 
         {
+            console.log(new Date)
             var currentEnergy = parseFloat($('div#energyBarT').html().split('/')[0])
             var maxEnergy = parseFloat($('div#energyBarT').html().split('/')[1])
-            console.log(currentEnergy + ' ' + maxEnergy)
+            console.log(currentEnergy +' < '+maxEnergy)
             if (currentEnergy < maxEnergy) {
+                
                 getRecoverableEnergy()
                 var random = generateRandomNumber()
-                console.log(recoverableEnergy + ' ' + random)
+                console.log(recoverableEnergy +' >= '+random)
                 if (recoverableEnergy >= random) {
                     $('#energyButton').trigger('click')
                 }
             } else {
+                console.log(new Date)
+                console.log('Done!')
                 undefined!==checkEnergyInterval&&clearInterval(checkEnergyInterval)
             }
         }
