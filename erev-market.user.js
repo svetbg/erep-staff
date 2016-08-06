d53a9464d9c05ed626868fe93e04185f55586993
svetbg <svetbg@users.noreply.github.com>
2016-08-06 13:32:17 +0300

    Update erev-market.user.js
    FIX: parsing price

--- erev-market.user.js
+++ erev-market.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Erev Market Improvements
 // @include      *www.erevollution.com/*/market/*/4/*
-// @version      0.0.1
+// @version      0.0.2
 // @description  Erev Market Improvements
 // @author       Anonymous
 // @grant        none
@@ -19,7 +19,7 @@ function style(t) {
     $( document ).ready(function() {
         $('div.main-box-body tr:gt(0)').each(function(){
             var priceTD = $(this).find('td.vs129')
-            var price = parseFloat(priceTD.find('strong').html())||0
+            var price = parseFloat(priceTD.find('strong').html().replace(' ',''))||0
             var usage = parseInt($(this).find('span.defense strong').html())||0
             var usagePerOneNrg = parseFloat(price/usage).toFixed(4)
             priceTD.html(priceTD.html()+'<br />'+usagePerOneNrg+' / nrg')
