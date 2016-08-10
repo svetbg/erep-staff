30136f3e4b65bec9e1a148990353e557e4c170cc
svetbg <svetbg@users.noreply.github.com>
2016-08-11 00:48:14 +0300

    Update homepage-improvements-dev.user.js
    WIP: Added referrals won gold.

--- homepage-improvements-dev.user.js
+++ homepage-improvements-dev.user.js
@@ -21,7 +21,7 @@ function style(t) {
         var checkEnergyInterval = setInterval(checkEnergy, 6*6e4)
         setTimeout(checkEnergy, 10e2)
         
-        improveMenu()
+        improveMenu(),improveReferralsPage()
         
         function improveMenu()
         {
@@ -33,6 +33,57 @@ function style(t) {
             storeMenuA.addClass('dropdown-toggle').append('<i class="fa fa-angle-right drop-icon"></i>')
             storeMenuItem.append('<ul class="submenu"><li><a href="'+storeMenuA.attr('href')+'">'+storeMenuA.find('span').html()+'</a></li><li><a href="'+storeMenuA.attr('href')+'#tab-2">Referrals</a></li></ul>')
             $('.nav-tabs a[href="' + location.hash + '"]').tab('show');
+        }
+        
+        function improveReferralsPage() {
+            if (location.hash.indexOf('#tab-2') < 0) {
+                return false
+            }
+            
+            var referralsDonated = JSON.parse(localStorage.getItem('erevRD'))||{};
+            referralsDonated['donated'] = referralsDonated['donated']||{}
+            
+            var infoTable = $('table.table')
+            infoTable.find('thead > tr').append('<th class="text-center">Already Collected</th>')
+            var playerTr = infoTable.find('tbody tr')
+            var form = infoTable.find('tbody tr').last().find('form')
+            var userInfo = []
+            var len = playerTr.length
+            playerTr.each(function(idx, el){
+                if (idx == len-1 && form.length > 0)
+                    return false
+                    
+                var row = $(this)
+                var userId = getUserIdFromUrl(row.find('td:gt(0) > a').attr('href'))
+                userInfo[userId] = parseFloat(row.find('td:eq(3) > strong').html())
+                referralsDonated['donated'][userId] = referralsDonated['donated'][userId]||0
+            })
+            
+            if (form.length > 0) {
+                for (var uId in userInfo) {
+                    if (userInfo.hasOwnProperty(uId)) {
+                        referralsDonated['donated'][uId] = referralsDonated['donated'][uId]||0
+                        referralsDonated['donated'][uId] += userInfo[uId]
+                    }
+                }
+            }
+            
+            playerTr.each(function(){
+                var row = $(this)
+                var userId = getUserIdFromUrl(row.find('td:gt(0) > a').attr('href'))
+                row.append('<td class="text-center"><strong>'+referralsDonated['donated'][userId].toLocaleString()+'</strong></td>')
+            })
+            
+            localStorage.setItem('erevRD',JSON.stringify(referralsDonated))
+        }
+        
+        function getUserIdFromUrl(url)
+        {
+            if (url === undefined)
+                return 0
+                
+            var infoArr = url.split('/')
+            return infoArr[infoArr.length-1]
         }
         
         function checkEnergy() 
