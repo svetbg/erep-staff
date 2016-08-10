37e373979330d13e4c7b059b5e7bb38387b2acbb
svetbg <svetbg@users.noreply.github.com>
2016-08-10 23:35:24 +0300

    Update homepage-improvements-dev.user.js
    Added Referrals menu item

--- homepage-improvements-dev.user.js
+++ homepage-improvements-dev.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Erev HomePage Improvements
-// @include      *www.erevollution.com/*/index
-// @version      0.0.5
+// @include      *www.erevollution.com*
+// @version      0.0.6
 // @description  Erev HomePage Improvements
 // @author       Anonymous
 // @grant        none
@@ -21,10 +21,28 @@ function style(t) {
         var checkEnergyInterval = setInterval(checkEnergy, 6*6e4)
         setTimeout(checkEnergy, 10e2)
         
+        improveMenu()
+        
+        function improveMenu()
+        {
+            var storeMenuItem = $('ul.nav-pills > li').eq(6)
+            if (storeMenuItem === undefined)
+                return False
+                
+            var storeMenuA = storeMenuItem.find('a')
+            storeMenuA.addClass('dropdown-toggle').append('<i class="fa fa-angle-right drop-icon"></i>')
+            storeMenuItem.append('<ul class="submenu"><li><a href="'+storeMenuA.attr('href')+'">'+storeMenuA.find('span').html()+'</a></li><li><a href="'+storeMenuA.attr('href')+'#tab-2">Referrals</a></li></ul>')
+            $('.nav-tabs a[href="' + location.hash + '"]').tab('show');
+        }
+        
         function checkEnergy() 
         {
-            var currentEnergy = parseFloat($('div#energyBarT').html().split('/')[0])
-            var maxEnergy = parseFloat($('div#energyBarT').html().split('/')[1])
+            var energyCont = $('div#energyBarT')
+            if (energyCont === undefined)
+                return False
+                
+            var currentEnergy = parseFloat(energyCont.html().split('/')[0])
+            var maxEnergy = parseFloat(energyCont.html().split('/')[1])
             if (currentEnergy < maxEnergy) {
                 getRecoverableEnergy()
                 var random = generateRandomNumber()
