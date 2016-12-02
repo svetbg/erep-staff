46f3f3df25c60dfa5c79135cace041d17fc4004a
svetbg <svetbg@users.noreply.github.com>
2016-12-02 18:49:40 +0200

    Update tentlan.user.js

--- tentlan.user.js
+++ tentlan.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tentlan
 // @include      *bg2.tentlan.com*
-// @version      0.0.1
+// @version      0.0.2
 // @description  Overview Improvements
 // @author       Anonymous
 // @grant        none
